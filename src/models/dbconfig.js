d0c1d2f47a3a500db186e8565674f9141bbcd00b
Megh Parikh <meghprkh@gmail.com>
2016-09-29 13:31:22 +0530

    Use mariadb in production

--- src/models/dbconfig.js
+++ src/models/dbconfig.js
@@ -1,8 +1,19 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'db.sqlite'
-});
+var sequelize;
+
+if (process.env.NODE_ENV == 'development') {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'db.sqlite'
+  });
+} else {
+  sequelize = new Sequelize('database', 'user', 'password', {
+    dialect: 'mariadb',
+    define: {
+      charset: 'utf8',
+    }
+  })
+}
 
 module.exports = sequelize;
